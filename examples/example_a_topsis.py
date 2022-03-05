ca58a1e901929a3afc73ed8e1c758d79df0c34ec
paaatcha <pacheco.comp@gmail.com>
2022-03-05 18:11:43 -0300

    atopsis almost done

--- examples/example_a_topsis.py
+++ examples/example_a_topsis.py
@@ -1,118 +1,21 @@
-# import sys
-# sys.path.append("../src")
-# from decision_making import TODIM
-#
-# td = TODIM([[1,2], [1,2]])
-# print(td.matrix_d)
-#
-# # -*- coding: utf-8 -*-
-# """
-# Author: Andre Pacheco
-# Email: pacheco.comp@gmail.com
-#
-# An examples of how to use the topsis class.
-#
-# """
-#
-# from __future__ import print_function
-# from __future__ import division
-# from __future__ import absolute_import
-#
-# from src.decision_making.topsis import TOPSISimport sys
-# sys.path.append("../src")
-# from decision_making import TODIM
-#
-# td = TODIM([[1,2], [1,2]])
-# print(td.matrix_d)
-#
-# # -*- coding: utf-8 -*-
-# """
-# Author: Andre Pacheco
-# Email: pacheco.comp@gmail.com
-#
-# An examples of how to use the topsis class.
-#
-# """
-#
-# from __future__ import print_function
-# from __future__ import division
-# from __future__ import absolute_import
-#
-# from src.decision_making.topsis import TOPSIS
-# import numpy as np
-#
-# A = TOPSIS('decisionMatrix.txt')
-# A.normalizeMatrix()
-# A.introWeights()
-# A.getIdealSolutions()
-# A.distanceToIdeal()
-# A.relativeCloseness()
-#
-# # Showing the results
-# print(A.rCloseness)
-# Alternatives = np.array(['A1', 'A2', 'A3'])
-# A.plotRankBar(Alternatives)
-#
-# # If you don't wanna use the file .txt, you can set the values
-# # as lists or numpy arrays
-#
-# w = np.array([0.3, 0.05, 0.6, 0.05])
-# cb = np.array([1, 0, 1, 0])
-# matrix = np.array([
-#     [15, 6, 25000, 7],
-#     [12, 7, 35000, 7],
-#     [10, 9, 55000, 8]
-# ])
-#
-# B = TOPSIS(matrix, w, cb)
-# B.normalizeMatrix()
-# B.introWeights()
-# B.getIdealSolutions()
-# B.distanceToIdeal()
-# B.relativeCloseness()
-#
-# # Showing the results
-# print(B.rCloseness)
-#
-#
-#
-#
-#
-# import numpy as np
-#
-# A = TOPSIS('decisionMatrix.txt')
-# A.normalizeMatrix()
-# A.introWeights()
-# A.getIdealSolutions()
-# A.distanceToIdeal()
-# A.relativeCloseness()
-#
-# # Showing the results
-# print(A.rCloseness)
-# Alternatives = np.array(['A1', 'A2', 'A3'])
-# A.plotRankBar(Alternatives)
-#
-# # If you don't wanna use the file .txt, you can set the values
-# # as lists or numpy arrays
-#
-# w = np.array([0.3, 0.05, 0.6, 0.05])
-# cb = np.array([1, 0, 1, 0])
-# matrix = np.array([
-#     [15, 6, 25000, 7],
-#     [12, 7, 35000, 7],
-#     [10, 9, 55000, 8]
-# ])
-#
-# B = TOPSIS(matrix, w, cb)
-# B.normalizeMatrix()
-# B.introWeights()
-# B.getIdealSolutions()
-# B.distanceToIdeal()
-# B.relativeCloseness()
-#
-# # Showing the results
-# print(B.rCloseness)
-#
-#
-#
-#
+"""
+This is a straightforward example of how to use the A-TOPSIS algorithm with this package.
+This example uses two approaches:
+1: using the decision matrices inside a csv files
+2: using the matrices in a list of lists
+"""
+
+import sys
+sys.path.append("../src")
+import numpy as np
+from decision_making import ATOPSIS
+
+
+atop = ATOPSIS("../test/avg_mat.csv", "../test/std_mat.csv", alg_col_name="Algorithms",
+               avg_cost_ben="cost", std_cost_ben="cost",
+               bench_col_names=["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11"])
+
+atop.get_ranking(True)
+
+
+
